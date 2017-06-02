8fce51de36f1fdc1174b4b9bcd1df4578f18504b
Antonia Haslinger, BSc <antonia.haslinger@gmx.at>
2017-06-02 12:31:12 +0200

    Worked on third level. Not fully working yet. (Fixing at tuesday)

---
+++ Assets/Scripts/PlayerInvisibiltyCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInvisibiltyCheck : MonoBehaviour {
+
+	public GameObject overlay;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Floor") {
+			if (other.gameObject.GetComponent<Renderer> ().material.name == "tile3active (Instance)") {
+				this.overlay.SetActive (true);
+			} 
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		this.overlay.SetActive (false);
+	}
+}
