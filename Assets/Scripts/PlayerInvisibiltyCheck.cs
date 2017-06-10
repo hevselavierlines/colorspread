1f8232d4059f9ee29b0627c34292a56493bd2a83
Antonia Haslinger, BSc <antonia.haslinger@gmx.at>
2017-06-10 17:23:31 +0200

    Edit Light, Added bot, Fixed bot area problem

--- Assets/Scripts/PlayerInvisibiltyCheck.cs
+++ Assets/Scripts/PlayerInvisibiltyCheck.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class PlayerInvisibiltyCheck : MonoBehaviour {
 
 	public GameObject overlay;
+	protected float time;
+	protected bool measureTime;
+	public float NEEDED_TIME = 0.4f;
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Floor") {
 			if (other.gameObject.GetComponent<Renderer> ().material.name == "tile3active (Instance)") {
 				this.overlay.SetActive (true);
+				this.measureTime = false;
 			} 
 		}
 	}
 
 	void OnTriggerExit(Collider other) {
-		this.overlay.SetActive (false);
+		this.measureTime = true;
+	}
+
+	void Update() {
+		if (measureTime == true) {
+			this.time += Time.deltaTime;
+			if (this.time >= NEEDED_TIME) {
+				this.overlay.SetActive (false);
+			}
+		} else {
+			this.time = 0.0f;
+		}
 	}
 }
